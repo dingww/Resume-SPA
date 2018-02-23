10f04b87a0fb291fa14070b4563dc0a74a8778d9
dingww <1067061570@qq.com>
2018-02-23 22:54:14 +0800

    education info

--- src/views/education.vue
+++ src/views/education.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-     dads   
+    <div class="education">
+        <div class="edu-cont">
+            <h3>2014.08 ~ 2017.07 <span>中山大学</span> 无线电物理专业</h3>
+            <div>
+                主要研究电磁波在复杂环境中的传播，发表过两篇论文，其中一篇被sci收录。
+            </div>
+        </div>
+        <div class="edu-cont">
+            <h3>2010.09 ~ 2014.07 <span>湖北师范大学</span> 电子信息工程专业</h3>
+            <div>
+                
+            </div>
+        </div>
     </div>
 </template>
 <script>
